521ce7187693e2b185159ff9674dd0864d226377
Matt Turco <matt.turco@gmail.com>
2018-02-10 15:57:41 -0600

    feat: style GroceryList and GroceryItem components

--- src/components/GroceryItem.js
+++ src/components/GroceryItem.js
@@ -5,7 +5,7 @@ export default class GroceryItem extends Component {
   render() {
     return (
       <div className="GroceryItem">
-        <label>
+        <label className="GroceryItem-label">
           <input type="checkbox" className="GroceryItem-checkbox" checked={this.props.checked} onChange={this.props.onToggle}/>
           <span className="GroceryItem-name">{this.props.item.name}</span>
           <span className="GroceryItem-qty">({this.props.item.qty})</span>
