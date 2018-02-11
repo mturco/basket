f0d4640e5094eac0201b893f63f7842ef40455dd
Matt Turco <matt.turco@gmail.com>
2018-02-10 18:17:42 -0600

    fix: item IDs aren't unique between sessions

--- src/models/GroceryItemModel.js
+++ src/models/GroceryItemModel.js
@@ -1,8 +1,10 @@
-let nextId = 0;
+function createId() {
+  return Math.random().toString(36).substr(2, 8);
+}
 
 export default class GroceryItemModel {
   constructor(name, qty) {
-    this.id = nextId++;
+    this.id = createId();
     this.name = name;
     this.qty = qty;
     this.picked = false;
